14cd221dc3b84b5567ccc5c359489e3962d39c16
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-01-05 11:47:01 -0300

    executou um simples script

---
+++ exemplo-node/script.js
@@ -0,0 +1,24 @@
+/*
+node -v
+node --version
+
+pwd = print working driectory => pwd imprime
+pwd imprime o diretoio altual
+
+criando uma pasta nova
+mkdir
+mkdir exemplo-node
+
+cd change driectory = mudar de diretório ou entra na pasta
+
+voltar para a pasta anterior
+cd..
+
+para abri no vs code, o ponto serve para abrir a pasta
+code .
+
+Executando arquivo no terminal o vs code
+node script.js
+*/
+
+console.log('meu primeiro programa node js');
